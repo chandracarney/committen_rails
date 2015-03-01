f8f133976e706593cb2a7949e94b60dcf8b0742e
Travis Yoder <trayoda@gmail.com>
2015-03-01 15:20:17 -0700

    it’s better to use quotes to find things in the session hash, since the symbol version can cause problems.
    session.clear is more explicit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,12 @@ class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by(uid: auth["uid"]) || User.create_with_github(auth)
-    session[:user_id] = user.id
-    redirect_to root_path, notice: "You're ready for some committen"
+    session["user_id"] = user.id
+    redirect_to root_path, notice: "You're ready for some committen."
   end
 
   def destroy
-    session[:user_id] = nil
+    session.clear
     redirect_to root_path, notice: "You've been signed out."
   end
 end
