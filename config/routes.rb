e810392454cb054f24e5d17aecab6bd7f2d40b9c
Travis Yoder <trayoda@gmail.com>
2015-03-01 15:14:05 -0700

    added an “auth/github” get so that we can use ‘auth_github_path’

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root "users#new"
+  root "home#index"
 
+  resources :users, only: [:index]
+
+  get "auth/github"
   get "auth/:provider/callback" => "sessions#create"
   get "signout" => "sessions#destroy", :as => :signout
 end
