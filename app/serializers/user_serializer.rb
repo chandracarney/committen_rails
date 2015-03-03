583cf619335ab84d12138358ab55f74121bf95ef
Travis Yoder <trayoda@gmail.com>
2015-03-03 10:55:32 -0700

    Generate some seeds for ember

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,4 @@
+class UserSerializer < ActiveModel::Serializer
+  attributes :name, :provider, :uid, :email,
+             :twitter, :thumbnail_url
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name: "travis",
+            provider: "github",
+            uid: "203489",
+            email: "travis@example.com",
+            thumbnail_url: "http://robohash.org/1"
+           )
+
+User.create(name: "sam",
+            provider: "github",
+            uid: "123456",
+            email: "sam@example.com",
+            thumbnail_url: "http://robohash.org/2"
+           )
+
+User.create(name: "chandra",
+            provider: "github",
+            uid: "567890",
+            email: "travis@example.com",
+            thumbnail_url: "http://robohash.org/3"
+           )
