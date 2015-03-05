1ab2796c8a1ba3bdd4aa75d5b9fce78b0731da7a
Sam Kuhlmann <samkuhlmann@gmail.com>
2015-03-05 16:49:07 -0700

    Cleans out the old rails github oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  root "home#index"
-
   namespace :api do
     resources :users
     resources :goals
     resources :commits
     resources :sessions, only: [:create]
   end
-
-  get "auth/github"
-  get "auth/:provider/callback" => "sessions#create"
-  get "signout" => "sessions#destroy", :as => :signout
 end
