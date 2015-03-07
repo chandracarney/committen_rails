73e7ee673134fe6905c500d5e5ed843a3e12e9f5
Travis Yoder <trayoda@gmail.com>
2015-03-07 15:12:45 -0700

    wrote a test for the create_with_github method on the user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,46 +1,60 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  describe "user attributes" do
-    it "must have a name" do
-      user = build(:user)
-      expect(user).to be_valid
+  describe "create with gihub" do
+    it "accepts a hash and builds a user" do
+      user = User.create_with_github(auth_data)
 
-      user.name = nil
+      expect(user.uid).to eq("1")
+    end
+  end
+
+  def auth_data
+    {
+      id: 1,
+      name: "peter parker",
+      login: "spiderman",
+      avatar_url: "http://robohash.org/1.png",
+      email: "spidey@example.com"
+    }
+  end
+
+  describe "validations" do
+    it "must have a name" do
+      user = build(:user, name: nil)
+
       expect(user).to_not be_valid
     end
 
     it "must have a provider" do
-      user = build(:user)
-      expect(user).to be_valid
+      user = build(:user, provider: nil)
 
-      user.provider = nil
       expect(user).to_not be_valid
     end
 
     it "must have a uid" do
-      user = build(:user)
-      expect(user).to be_valid
+      user = build(:user, uid: nil)
 
-      user.uid = nil
       expect(user).to_not be_valid
     end
 
     it "must have a nickname" do
-      user = build(:user)
-      expect(user).to be_valid
+      user = build(:user, nickname: nil)
 
-      user.nickname = nil
       expect(user).to_not be_valid
     end
+  end
 
+  describe "relationships" do
     it "has many commits" do
       user = build(:user)
+
       expect(user).to respond_to(:commits)
     end
 
     it "has many goals" do
       user = build(:user)
+
       expect(user).to respond_to(:goals)
     end
   end
